06a5601aeb8f6ab924a93bbe4f9f218368d261a3
be3win <be3win@empal.com>
2016-11-28 21:32:17 +0900

    "added output 'Tell his World'"

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 class Hello {
 public static void main(String[] args) {
-System.out.println("Hello World");
-Systme.out.println("Hello Your World");
+	System.out.println("Hello World");
+	System.out.println("Hello Your World");
+	System.out.println("Tell his world");
 }
 }
