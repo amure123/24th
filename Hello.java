6fb4354f4a5a53d5eb0701efd548a3cf747dd0a1
Patrick <ppkwak@gmail.com>
2016-11-30 21:39:23 +0900

    GitHub for git pull

--- Hello.java
+++ Hello.java
@@ -7,5 +7,6 @@ public static void main(String[] args) {
 	System.out.println("Hello World");
 	System.out.println("Hello Your World");
 	System.out.println("Tell his world");
+	System.out.println("Tell my world");
 }
 }
