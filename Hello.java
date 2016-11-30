c882349f5336c52f6416788aa0b876579864d3b1
be3win <be3win@empal.com>
2016-11-30 21:20:19 +0900

    Hello.java modified on Local repository

--- Hello.java
+++ Hello.java
@@ -1,4 +1,7 @@
-class Hello {
+class Hello 
+{
+	//For command line git tutorial
+	//Local repository modification
 public static void main(String[] args) {
 	System.out.println("Hello World");
 	System.out.println("Hello Your World");
