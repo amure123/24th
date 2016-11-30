67ddff5b9755f91dcb95acc5fc20cdf0a76258cd
Patrick <ppkwak@gmail.com>
2016-11-30 21:12:01 +0900

    Hello.java modified on GitHub

--- Hello.java
+++ Hello.java
@@ -1,8 +1,9 @@
 class Hello {
+	//For command line git tutorial
+	//GitHub modification
 public static void main(String[] args) {
 	System.out.println("Hello World");
 	System.out.println("Hello Your World");
-
 	System.out.println("Tell his world");
 }
 }
