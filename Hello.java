a29e1e77eeff91eb86219de1019e0d8ef4ee7e54
be3win <be3win@empal.com>
2016-11-30 19:31:54 +0900

    conflict resolved

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ class Hello {
 public static void main(String[] args) {
 System.out.println("Hello World");
 System.out.println("Tell Your World");
-System.out.println("Tell her World");"
+System.out.println("Tell her World");
 }
 }
